1e267cd36f9f22627ce7cba8f22c03c4c8ee86fb
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-01-05 18:18:57 +0200

    Added a random computer opponent

--- index.js
+++ index.js
@@ -6,11 +6,27 @@ console.log(cells);
 cells.forEach(cell => cell.addEventListener('click', () => {
     if (filled[cells.indexOf(cell)] == -1){
         filled[cells.indexOf(cell)] = 1;
-        cell.innerHTML = '0'
-        computer_turn();
+        cell.innerHTML = '0';
+        if (!check_win(1)){
+            computer_turn();
+        }
     }
 }));
 
 function computer_turn(){
-    // Insert logic for computer's turn here
+    // We will replace this with an AI that uses the Minimax algorithm
+    x = Math.floor(Math.random() * 9)
+    while (filled[x] != -1){
+        x = Math.floor(Math.random() * 9)
+        console.log(x);
+    }
+    filled[x] = 0;
+    cells[x].innerHTML = 'X';
+    if (check_win(0)){
+        //Insert what to do if AI wins here
+    }
+}
+
+function check_win(player){
+    //Write code here that takes a player (either 0 or 1) and returns if true if they won and false if they didn't
 }

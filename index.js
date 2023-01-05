1204341ade264030226c7ffe7c20868a6b6b2ac6
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-01-05 20:09:32 +0200

    Added win/lose conditions

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const cells = Array.from(document.querySelectorAll('.cell'));
 const filled = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
+const win_combs = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
 
 console.log(cells);
 
@@ -9,6 +19,9 @@ cells.forEach(cell => cell.addEventListener('click', () => {
         cell.innerHTML = '0';
         if (!check_win(1)){
             computer_turn();
+        }
+        else {
+            console.log('WIN!'); //Implement player win logic here
         }
     }
 }));
@@ -23,10 +36,16 @@ function computer_turn(){
     filled[x] = 0;
     cells[x].innerHTML = 'X';
     if (check_win(0)){
-        //Insert what to do if AI wins here
+        //Display lose and pause game
     }
 }
 
 function check_win(player){
     //Write code here that takes a player (either 0 or 1) and returns if true if they won and false if they didn't
+    for (let comb of win_combs){
+        if (filled[comb[0]] == player && filled[comb[1]] == player && filled[comb[2]] == player){
+            return true
+        }
+    }
+    return false
 }
